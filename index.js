c982e4d71b7cae2b4338aced7a14ac29b0e720a2
justianromuel <jastinromuel@gmail.com>
2022-07-16 22:45:15 +0700

    update

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ require('dotenv').config()
 
 const express = require('express')
 
+const cors = require('cors')
+
+const http = require('http')
+
 const router = require('./src/routes')
 
 const app = express()
 
-const port = 5000
+const server = http.createServer(app)
+
+const port = process.env.PORT || 5000
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/v1/', router)
 
